5b8ab92ef2189e8e49406c9cf21adb458eebb464
Rohit Voleti <rnvoleti@asu.edu>
2019-01-02 04:30:46 -0700

    Fixed the WER implementation to not replace all words for a given substitution. Should check sentence embedding results to see if and how they may have changed.

--- src/tools/04_corrupt_sentences.py
+++ src/tools/04_corrupt_sentences.py
@@ -8,12 +8,9 @@ Sentence = phono_module.Sentence
 STOP = phono_module.STOP
 
 
-def identify_subs(tokens, error_rate):
-    x = {word: np.random.rand() for word in tokens}
-    filtered = {k: v for k, v in x.items() if v < error_rate}
-
-    # Return randomly selected tokens list
-    return list(filtered.keys())
+def identify_subs(tokens):
+    x = [(word, np.random.rand()) for word in tokens]
+    return x
 
 
 def pick_similar_word(prob_df):
@@ -29,16 +26,26 @@ def replace_error_words(probs_df, sentence, error_rate):
 
     tokens = sentence_class.tokens_cased
 
-    words_to_replace = identify_subs(tokens, error_rate=error_rate)
+    words_to_replace = identify_subs(tokens)
 
-    probs = probs_df.loc[(words_to_replace, slice(None)), :]
+    corrupt_sent = []
+    for word, num in words_to_replace:
+        if word in probs_df.index.levels[0] and num < error_rate:  # replace word
+            probs = probs_df.loc[(word, slice(None)), :]
+            corrupt_sent.append(pick_similar_word(probs))
+        else:
+            corrupt_sent.append(word)
 
-    probs_group = probs.groupby(level='Corpus Word')
+    sentence_class.tokens_cased = corrupt_sent
+    # words_only = [word for word, prob in words_to_replace]
 
-    sim_words_dict = probs_group.apply(pick_similar_word).to_dict()
+    # probs = probs_df.loc[(words_only, slice(None)), :]
+    #
+    # probs_group = probs.groupby(level='Corpus Word')
+    # sim_words_dict = probs_group.apply(pick_similar_word).to_dict()
 
-    for k, v in list(sim_words_dict.items()):
-        sentence_class.tokens_cased = [re.sub(r"\b" + k + r"\b", v, w) for w in sentence_class.tokens_cased]
+    # for k, v in list(sim_words_dict.items()):
+    #    sentence_class.tokens_cased = [re.sub(r"\b" + k + r"\b", v, w) for w in sentence_class.tokens_cased]
 
     sentence_class.tokens = [w.lower() for w in sentence_class.tokens_cased]
     sentence_class.tokens_without_stop = [w for w in sentence_class.tokens if w not in STOP]
@@ -50,7 +57,7 @@ def replace_error_words(probs_df, sentence, error_rate):
 
 
 if __name__ == '__main__':
-    sample_sentence = 'ice cream is the best dessert in the world as we know it.'
+    sample_sentence = 'ice cream is the best dessert in the world as we know it is it is it is it is.'
     wer = 0.50
     with open('../models/test_probs_df.pkl', 'rb') as f:
         probs = pickle.load(f)
